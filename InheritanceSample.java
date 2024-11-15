d45932b42a0cdce52ccebcf53cd653b23ca548b9
Asper The Ghost <23-19300@g.batstate-u.edu.ph>
2024-11-15 12:48:00 +0800

    Update and rename InheritanceExample.java to InheritanceSample.java

--- InheritanceSample.java
+++ InheritanceSample.java
@@ -1,4 +1,4 @@
-public class InheritanceExample {
+public class InheritanceSample {
     public static void main(String[] args){
 
         System.out.println("--------------Student-------------------");
